7ba1ecbef540772669f9aa988ad77bf6ef330be3
Darian Nocera <darnocer@gmail.com>
2024-03-27 11:29:48 -0600

    initial theme configuration

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,51 @@
 import React from "react";
+import { useRouter } from "next/router";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
+import Logo from "./components/logo";
+import Metadata from "./components/metadata";
+import TocCallout from "./components/toccallout";
+
 const config: DocsThemeConfig = {
-  logo: <span>Microdosing 101</span>,
+  logo: <Logo />,
   project: {
-    link: "https://github.com/shuding/nextra-docs-template",
+    link: "https://github.com/darnocer/microdose-faq",
   },
-  chat: {
-    link: "https://discord.com",
-  },
-  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  docsRepositoryBase: "https://github.com/darnocer/microdose-faq",
   footer: {
     text: "Microdosing.wiki",
+  },
+  useNextSeoProps() {
+    const router = useRouter();
+    if (router.asPath !== "/") {
+      return {
+        titleTemplate: "%s | Microdosing 101",
+      };
+    }
+    return {};
+  },
+  head: <Metadata />,
+  primaryHue: 281,
+  primarySaturation: 90,
+  search: {
+    placeholder: "Search",
+  },
+  banner: {
+    key: "welcome1",
+    text: "🍄 Welcome to micodose.wiki!",
+  },
+  sidebar: {
+    toggleButton: true,
+  },
+  toc: {
+    extraContent: <TocCallout />,
+  },
+  editLink: {
+    text: "",
+    component: null,
+  },
+  feedback: {
+    content: null,
   },
 };
 
