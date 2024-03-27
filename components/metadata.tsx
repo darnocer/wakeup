014489a07871fb317be3da9c0bb5c4b8363410e3
Darian Nocera <darnocer@gmail.com>
2024-03-27 11:29:33 -0600

    create custom components: - logo - metadata - toc callout

---
+++ components/metadata.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Metadata = () => (
+  <>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta property="og:title" content="Microdosing 101" />
+    <meta
+      property="og:description"
+      content="FAQ for getting started microdosing with psilocybin mushrooms."
+    />
+  </>
+);
+
+export default Metadata;
---
+++ components/toccallout.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TocCallout = () => (
+  <>
+    <span className="text-xl text-red-500">Testing</span>
+  </>
+);
+
+export default TocCallout;
