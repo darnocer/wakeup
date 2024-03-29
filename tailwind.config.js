c1d4b9648fda10e8cf928d3d812b3f4923af6d73
Darian Nocera <darnocer@gmail.com>
2024-03-29 10:25:54 -0600

    add global font styles and update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,jsx,ts,tsx,md,mdx}",
     "./components/**/*.{js,jsx,ts,tsx,md,mdx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Rubik", "sans-serif", ...defaultTheme.fontFamily.sans],
+    },
+    extend: {
+      colors: {
+        neutral: {
+          500: "#1b2024",
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: ["@tailwindcss/typography"],
 };
