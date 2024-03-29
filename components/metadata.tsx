ec955952076dc31503381fe7158c2180ab782909
Darian Nocera <darnocer@gmail.com>
2024-03-29 11:24:27 -0600

    add favicon

--- components/metadata.tsx
+++ components/metadata.tsx
@@ -3,11 +3,12 @@ import React from "react";
 const Metadata = () => (
   <>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta property="og:title" content="Microdosing 101" />
+    <meta property="og:title" content="Microdose Wiki" />
     <meta
       property="og:description"
       content="FAQ for getting started microdosing with psilocybin mushrooms."
     />
+    <link rel="icon" href="/favicon.ico" />
   </>
 );
 
