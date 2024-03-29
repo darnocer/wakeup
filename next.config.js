26e8206a6bbebc30e8bdf55033ec538e042d76f0
Darian Nocera <darnocer@gmail.com>
2024-03-29 11:44:16 -0600

    exclude .obsidian from build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  exclude: ["./.next/server/pages/.obsidian"],
+});
 
-module.exports = withNextra()
+module.exports = withNextra();
