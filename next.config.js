e07b9a1fe00bfc835f6d217045e804934c04778e
Darian Nocera <darnocer@gmail.com>
2024-06-05 09:11:19 -0600

    update next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
   exclude: ["./.next/server/pages/.obsidian"],
+  standalone: true,
 });
 
 module.exports = withNextra();
